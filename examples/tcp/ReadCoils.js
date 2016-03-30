cf90fb2d419825c20e34b0bf6db7416cb75fa5d6
stefan.poeter@cloud-automation.de <stefan.poeter@cloud-automation.de>
2016-03-30 11:48:52 +0200

    Fixed Read Coils Example. Switched to stampit-log#0.2.0 for better logging.

--- examples/tcp/ReadCoils.js
+++ examples/tcp/ReadCoils.js
@@ -1,5 +1,5 @@
 var modbus  = require('../../'),
-    client  = modbus.client.tcp.complete({ 'host' : process.argv[2],  'port' : process.argv[3]});
+    client  = modbus.client.tcp.complete({ 'host' : process.argv[2],  'port' : process.argv[3] , 'logEnabled' : true});
 
 client.on('connect', function () {
 
@@ -25,3 +25,5 @@ client.on('error', function (err) {
     console.log(err);
 
 });
+
+client.connect();
