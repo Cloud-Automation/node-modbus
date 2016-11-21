41327d1c4efebf9c7cd85f243add1835cd0bc483
GermanBluefox <dogafox@gmail.com>
2016-11-21 19:00:17 +0100

    Fix error in the CRC calculation and add unitID back

--- src/modbus-serial-client.js
+++ src/modbus-serial-client.js
@@ -132,16 +132,9 @@ module.exports = stampit()
         }.bind(this);
     
         var onSend = function (pdu) {
-
-            var crc = 0;
-
             var base = Buffer.allocUnsafe(1);
-            base.writeUInt8(1);
+            base.writeUInt8(this.unitId);
             var buf = Buffer.concat([base, pdu]);
-
-            for (var i = 0; i < buf.length; i += 1) {
-                crc = (buf.readUInt8(i) + crc) % 0xFFFF; 
-            }
 
             var crc16 = crc.crc16modbus(buf);
 
