180cb5f142a47f572f304c7f78845a1b3815fb59
stefan.poeter@cloud-automation.de <stefan.poeter@cloud-automation.de>
2016-11-24 11:21:17 +0100

    - Adopted the new promise API to the WriteSingleRegister.js - Applied standard javascript code style.

--- examples/tcp/WriteSingleRegister.js
+++ examples/tcp/WriteSingleRegister.js
@@ -1,33 +1,18 @@
-var modbus  = require('../..'),
-    client = modbus.client.tcp.complete({
-        'host'  : process.argv[2], 
-        'port'  : process.argv[3],
-        'logEnabled' : true,
-        'logLevel'   : 'debug' });
+var modbus = require('../..')
+var client = modbus.client.tcp.complete({
+  'host': process.argv[2],
+  'port': process.argv[3],
+  'logEnabled': true,
+  'logLevel': 'debug' })
 
 // override logger function
-client.on('connect', function () { 
-    
-    client.writeSingleRegister(process.argv[4], process.argv[5]).then(function (resp) {
+client.on('connect', function () {
+  client.writeSingleRegister(process.argv[4], process.argv[5]).then(function (resp) {
+    console.log(resp)
+  }, console.error).done(function () {
+    client.close()
+  })
+})
 
-        console.log(resp);
-
-    }).fail(function (err) {
- 
-        console.log(err);
-
-    }).done(function () {
-    
-        client.close();
-    
-    });
-
-});
-
-client.on('error', function (err) {
-
-    console.log(err);
-
-});
-
-client.connect();
+client.on('error', console.error)
+client.connect()
