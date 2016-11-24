9be59616d4e2b71712c2ce1b4e2bf7863e447faf
stefan.poeter@cloud-automation.de <stefan.poeter@cloud-automation.de>
2016-11-24 11:25:55 +0100

    - Applied standard javascript code style to serial examples. - Adopted new promise API to WriteSingleCoil example   and applied standard javascript code style.

--- examples/serial/WriteSingleRegister.js
+++ examples/serial/WriteSingleRegister.js
@@ -1,26 +1,16 @@
-var ModbusClient  = require('../..'),
-    client = ModbusClient.createSerialClient('/dev/tty0', 9600);
+var ModbusClient = require('../..')
+var client = ModbusClient.createSerialClient('/dev/tty0', 9600)
 
-client.on('connect', function () { 
-    
-    client.writeSingleRegister(5, 123).then(function (resp) {
-
-        console.log(resp);
-
-    }).fail(function (err) {
- 
-        console.log(err);
-
-    }).done(function () {
-    
-        client.close();
-    
-    });
-
-});
+client.on('connect', function () {
+  client.writeSingleRegister(5, 123).then(function (resp) {
+    console.log(resp)
+  }).fail(function (err) {
+    console.log(err)
+  }).done(function () {
+    client.close()
+  })
+})
 
 client.on('error', function (err) {
-
-    console.log(err);
-
-});
+  console.log(err)
+})
