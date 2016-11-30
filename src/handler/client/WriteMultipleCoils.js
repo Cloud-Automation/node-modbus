078f5540671890dfce01bdb9db060ecb41c9040d
stefan.poeter@cloud-automation.de <stefan.poeter@cloud-automation.de>
2016-11-30 13:19:02 +0100

    - Removed deprecated Promise.defer from WriteMultipleCoils handler.

--- src/handler/client/WriteMultipleCoils.js
+++ src/handler/client/WriteMultipleCoils.js
@@ -31,51 +31,50 @@ module.exports = stampit()
     }.bind(this)
 
     this.writeMultipleCoils = function (startAddress, coils, N) {
-      var defer = Promise.defer()
-      var fc = 15
-      var basePdu = Buffer.allocUnsafe(6)
-      var pdu
+      return new Promise(function (resolve, reject) {
+        var fc = 15
+        var basePdu = Buffer.allocUnsafe(6)
+        var pdu
 
-      basePdu.writeUInt8(fc, 0)
-      basePdu.writeUInt16BE(startAddress, 1)
+        basePdu.writeUInt8(fc, 0)
+        basePdu.writeUInt16BE(startAddress, 1)
 
-      if (coils instanceof Buffer) {
-        basePdu.writeUInt16BE(N, 3)
-        basePdu.writeUInt8(coils.length, 5)
-        pdu = Buffer.concat([basePdu, coils])
-      } else if (coils instanceof Array) {
-        if (coils.length > 1968) {
-          defer.reject()
-          return
-        }
-
-        var byteCount = Math.ceil(coils.length / 8)
-        var curByte = 0
-        var curByteIdx = 0
-        var cntr = 0
-        var payloadPdu = Buffer.allocUnsafe(byteCount)
-
-        basePdu.writeUInt16BE(coils.length, 3)
-        basePdu.writeUInt8(byteCount, 5)
-
-        for (var i = 0; i < coils.length; i += 1) {
-          curByte += coils[i] ? Math.pow(2, cntr) : 0
-
-          cntr = (cntr + 1) % 8
-
-          if (cntr === 0 || i === coils.length - 1) {
-            payloadPdu.writeUInt8(curByte, curByteIdx)
-            curByteIdx = curByteIdx + 1
-            curByte = 0
+        if (coils instanceof Buffer) {
+          basePdu.writeUInt16BE(N, 3)
+          basePdu.writeUInt8(coils.length, 5)
+          pdu = Buffer.concat([basePdu, coils])
+        } else if (coils instanceof Array) {
+          if (coils.length > 1968) {
+            reject()
+            return
           }
+
+          var byteCount = Math.ceil(coils.length / 8)
+          var curByte = 0
+          var curByteIdx = 0
+          var cntr = 0
+          var payloadPdu = Buffer.allocUnsafe(byteCount)
+
+          basePdu.writeUInt16BE(coils.length, 3)
+          basePdu.writeUInt8(byteCount, 5)
+
+          for (var i = 0; i < coils.length; i += 1) {
+            curByte += coils[i] ? Math.pow(2, cntr) : 0
+
+            cntr = (cntr + 1) % 8
+
+            if (cntr === 0 || i === coils.length - 1) {
+              payloadPdu.writeUInt8(curByte, curByteIdx)
+              curByteIdx = curByteIdx + 1
+              curByte = 0
+            }
+          }
+
+          pdu = Buffer.concat([basePdu, payloadPdu])
         }
 
-        pdu = Buffer.concat([basePdu, payloadPdu])
-      }
-
-      this.queueRequest(fc, pdu, defer)
-
-      return defer.promise
+        this.queueRequest(fc, pdu, { resolve: resolve, reject: reject })
+      }.bind(this))
     }
 
     init()
