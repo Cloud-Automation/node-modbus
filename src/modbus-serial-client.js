bcffffbad2a5234a9e81e6c1ade353375bef5a3c
Armin Beck <armin_beck@web.de>
2016-12-07 21:40:26 +0100

    Removed variable crc from onSend function
    remove crc = 0 which overwriting global var crc = require('crc')
    Else call crc.crc16modbus(buf) will fail

--- src/modbus-serial-client.js
+++ src/modbus-serial-client.js
@@ -87,7 +87,8 @@ module.exports = stampit()
                     }
                 }
               }
-          }
+           }
+        }
       }
     }.bind(this)
 
@@ -96,16 +97,11 @@ module.exports = stampit()
     }.bind(this)
 
     var onSend = function (pdu) {
-      var crc = 0
+      
 
       var base = Buffer.allocUnsafe(1)
       base.writeUInt8(1)
       var buf = Buffer.concat([base, pdu])
-
-      for (var i = 0; i < buf.length; i += 1) {
-        crc = (buf.readUInt8(i) + crc) % 0xFFFF
-      }
-
       var crc16 = crc.crc16modbus(buf)
       var crcBuf = Buffer.allocUnsafe(2)
 
