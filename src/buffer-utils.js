ed7961bf36ab01683b79150e7e1c10f5a280b0a3
Unknown <anthony.mujic@gmail.com>
2018-02-11 10:52:47 +1100

    Converted buffer utils to a class.

--- src/buffer-utils.js
+++ src/buffer-utils.js
@@ -25,73 +25,72 @@ let debug = require('debug')('buffer-utils')
 * @returns shifted output buffer
 */
 
-function bufferShift(start_address, end_address, outputs) {
-  start_address = start_address - 1
-  let start_shift = start_address % 8
-  let start_byte = Math.floor(start_address / 8)
-  let end_byte = Math.floor(end_address / 8)
+class BufferUtils {
 
-  let size = end_byte - start_byte + 1
-  let remainder = 8 - start_shift
+    static bufferShift(start_address, end_address, outputs) {
+      start_address = start_address - 1
+      let start_shift = start_address % 8
+      let start_byte = Math.floor(start_address / 8)
+      let end_byte = Math.floor(end_address / 8)
 
-  // Define a new buffer
-  let buffer = Buffer.allocUnsafe(size)
+      let size = end_byte - start_byte + 1
+      let remainder = 8 - start_shift
 
-  buffer[0] = outputs[0] << start_shift
-  debug('buffer[0] = %s ( %s << %d )', buffer[0].toString(2), outputs[0].toString(2), start_shift)
+      // Define a new buffer
+      let buffer = Buffer.allocUnsafe(size)
 
-  let padded_buffer = Buffer.concat([outputs, Buffer.alloc(1)], outputs.length + 1)
+      buffer[0] = outputs[0] << start_shift
+      debug('buffer[0] = %s ( %s << %d )', buffer[0].toString(2), outputs[0].toString(2), start_shift)
 
-  for (let i = 1; i < size; i++) {
-      buffer[i] = (padded_buffer[i] << start_shift) + (padded_buffer[i - 1] >> (8 - start_shift))
-      debug('buffer[%d] = %s ( %s << %d + %s >> %d)',
-                    i,
-                    buffer[i].toString(2),
-                    padded_buffer[i].toString(2),
-                    start_shift,
-                    padded_buffer[i-1].toString(2),
-                    8 - start_address
-                )
-  }
+      let padded_buffer = Buffer.concat([outputs, Buffer.alloc(1)], outputs.length + 1)
 
-  return buffer
+      for (let i = 1; i < size; i++) {
+          buffer[i] = (padded_buffer[i] << start_shift) + (padded_buffer[i - 1] >> (8 - start_shift))
+          debug('buffer[%d] = %s ( %s << %d + %s >> %d)',
+                        i,
+                        buffer[i].toString(2),
+                        padded_buffer[i].toString(2),
+                        start_shift,
+                        padded_buffer[i-1].toString(2),
+                        8 - start_address
+                    )
+      }
+
+      return buffer
+    }
+
+    /** firstByte ensure first byte is set correctly
+    * @param {start_address} first coil to write
+    * @param {origianl_byte} byte from the original coils buffer
+    * @param {output_byte} first byte from the shifted outputs buffer
+    * @returns correct first byte to be written to coils buffer
+    */
+    static firstByte(start_address, original_byte, output_byte) {
+        start_address = start_address - 1
+        let start_shift = start_address % 8
+        let mask = 0xff >> (8 - start_shift)
+        let masked_original_byte = original_byte & mask
+
+        let first_byte = output_byte + masked_original_byte
+
+        return first_byte
+    }
+
+    /** lastByte ensure last byte is set correctly
+    * @param {end_address} last coil to write
+    * @param {origianl_byte} byte from the original coils buffer
+    * @param {output_byte} last byte from the shifted outputs buffer
+    * @returns correct last byte to be written to coils buffer
+    */
+    static lastByte(end_address, original_byte, output_byte) {
+        let end_shift = end_address % 8
+        let mask = 0xff << end_shift
+        let masked_original_byte = original_byte & mask
+
+        let last_byte = output_byte + masked_original_byte
+
+        return last_byte
+    }
 }
 
-/** firstByte ensure first byte is set correctly
-* @param {start_address} first coil to write
-* @param {origianl_byte} byte from the original coils buffer
-* @param {output_byte} first byte from the shifted outputs buffer
-* @returns correct first byte to be written to coils buffer
-*/
-function firstByte(start_address, original_byte, output_byte) {
-    start_address = start_address - 1
-    let start_shift = start_address % 8
-    let mask = 0xff >> (8 - start_shift)
-    let masked_original_byte = original_byte & mask
-
-    let first_byte = output_byte + masked_original_byte
-
-    return first_byte
-}
-
-/** lastByte ensure last byte is set correctly
-* @param {end_address} last coil to write
-* @param {origianl_byte} byte from the original coils buffer
-* @param {output_byte} last byte from the shifted outputs buffer
-* @returns correct last byte to be written to coils buffer
-*/
-function lastByte(end_address, original_byte, output_byte) {
-    let end_shift = end_address % 8
-    let mask = 0xff << end_shift
-    let masked_original_byte = original_byte & mask
-
-    let last_byte = output_byte + masked_original_byte
-
-    return last_byte
-}
-
-module.exports = {
-                    bufferShift,
-                    firstByte,
-                    lastByte
-                }
+module.exports = BufferUtils
