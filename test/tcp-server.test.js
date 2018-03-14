9994840b594e39e8c4d3088db7d8020363e853b3
Renan Garcia <renan.nobregarcia@gmail.com>
2018-03-14 11:39:39 -0300

    Extended testing. Inclusion of valid response buffer checking

--- test/tcp-server.test.js
+++ test/tcp-server.test.js
@@ -11,37 +11,38 @@ describe('TCP Server Tests.', function () {
 
   beforeEach(function () {
     socket = new EventEmitter()
-    socket.write = function (response) { }
+    socket.write = (response) => {}
 
     server = new Modbus.server.TCP(socket, {
-        coils: Buffer.from([0x55, 0x55, 0x55])
+      holding: Buffer.alloc(12, 0x00),
+      coils: Buffer.from([0x55, 0x55, 0x55])
     })
   })
 
   describe('Write Single Coil Tests.', function () {
     it('should force a coil in the server buffer at address 1', function (done) {
-      let request = Buffer.from([
+      const request = Buffer.from([
         0x00, 0x01, // transaction id
         0x00, 0x00, // protocol
         0x00, 0x06, // byte count
         0x02, // unit id
         0x05, // function code
-        0x00, 0x01, // output address
-        0xFF, 0x00 // output value
+        0x00, 0x01, // address
+        0xFF, 0x00 // value
       ])
+      const expectedCoils = Buffer.from([0x57, 0x55, 0x55])
 
-      socket.write = function (response) {
-          assert.deepEqual(request, response)
-          assert.deepEqual(Buffer.from([0x57, 0x55, 0x55]), server.coils)
-          done()
+      socket.write = (response) => {
+        assert.deepEqual(request, response)
+        assert.deepEqual(expectedCoils, server.coils)
+        done()
       }
 
       socket.emit('connection', socket)
       socket.emit('data', request)
-
     })
     it('should force a coil in the server buffer at address 8', function (done) {
-      let request = Buffer.from([
+      const request = Buffer.from([
         0x00, 0x01, // transaction id
         0x00, 0x00, // protocol
         0x00, 0x06, // byte count
@@ -50,114 +51,177 @@ describe('TCP Server Tests.', function () {
         0x00, 0x08, // output address
         0x00, 0x00 // output value
       ])
+      const expectedCoils = Buffer.from([0x55, 0x54, 0x55])
 
-      socket.write = function (response) {
-          assert.deepEqual(request, response)
-          assert.deepEqual(Buffer.from([0x55, 0x54, 0x55]), server.coils)
-          done()
+      socket.write = (response) => {
+        assert.deepEqual(request, response)
+        assert.deepEqual(expectedCoils, server.coils)
+        done()
       }
 
       socket.emit('connection', socket)
       socket.emit('data', request)
-
     })
   })
 
   describe('Write Multiple Coils Tests.', function () {
-    it('should correctly write <0F> in the server buffer coils at address 0', function (done) {
-      let request = Buffer.from([
+    it('should write <0F> in the server buffer coils at address 0', function (done) {
+      const request = Buffer.from([
         0x00, 0x01, // transaction id
         0x00, 0x00, // protocol
         0x00, 0x08, // byte count
-        0x02,       // unit id
-        0x0F,       // function code
+        0x02, // unit id
+        0x0F, // function code
         0x00, 0x00, // address
         0x00, 0x04, // quantity
-        0x01,       // byte count
-        0x0F        // values
+        0x01, // byte count
+        0x0F // values
       ])
+      const expectedResponse = Buffer.from([
+        0x00, 0x01, // transaction id
+        0x00, 0x00, // protocol
+        0x00, 0x06, // byte count
+        0x02, // unit id
+        0x0F, // function code
+        0x00, 0x00, // address
+        0x00, 0x04, // quantity
+      ])
+      const expectedCoils = Buffer.from([0x5F, 0x55, 0x55])
+
+      socket.write = (response) => {
+        assert.deepEqual(expectedResponse, response)
+        assert.deepEqual(expectedCoils, server.coils)
+        done()
+      }
 
       socket.emit('connection', socket)
       socket.emit('data', request)
-
-      assert.deepEqual(Buffer.from([0x5F, 0x55, 0x55]), server.coils)
-      done()
     })
-    it('should correctly write <FF> in the server buffer coils at address 0', function (done) {
-      let request = Buffer.from([
+    it('should write <FF> in the server buffer coils at address 0', function (done) {
+      const request = Buffer.from([
         0x00, 0x01, // transaction id
         0x00, 0x00, // protocol
         0x00, 0x08, // byte count
-        0x02,       // unit id
-        0x0F,       // function code
+        0x02, // unit id
+        0x0F, // function code
         0x00, 0x00, // address
         0x00, 0x08, // quantity
-        0x01,       // byte count
-        0xFF        // values
+        0x01, // byte count
+        0xFF // values
       ])
-      
+      const expectedResponse = Buffer.from([
+        0x00, 0x01, // transaction id
+        0x00, 0x00, // protocol
+        0x00, 0x06, // byte count
+        0x02, // unit id
+        0x0F, // function code
+        0x00, 0x00, // address
+        0x00, 0x08, // quantity
+      ])
+      const expectedCoils = Buffer.from([0xFF, 0x55, 0x55])
+
+      socket.write = (response) => {
+        assert.deepEqual(expectedResponse, response)
+        assert.deepEqual(expectedCoils, server.coils)
+        done()
+      }
+
       socket.emit('connection', socket)
       socket.emit('data', request)
-
-      assert.deepEqual(Buffer.from([0xFF, 0x55, 0x55]), server.coils)
-      done()
     })
-    it('should correctly write <FF 01> in the server buffer coils at address 0', function (done) {
-      let request = Buffer.from([
+    it('should write <FF 01> in the server buffer coils at address 0', function (done) {
+      const request = Buffer.from([
         0x00, 0x01, // transaction id
         0x00, 0x00, // protocol
         0x00, 0x09, // byte count
-        0x02,       // unit id
-        0x0F,       // function code
+        0x02, // unit id
+        0x0F, // function code
         0x00, 0x00, // address
         0x00, 0x09, // quantity
-        0x02,       // byte count
-        0xFF, 0x01  // values
+        0x02, // byte count
+        0xFF, 0x01 // values
       ])
+      const expectedResponse = Buffer.from([
+        0x00, 0x01, // transaction id
+        0x00, 0x00, // protocol
+        0x00, 0x06, // byte count
+        0x02, // unit id
+        0x0F, // function code
+        0x00, 0x00, // address
+        0x00, 0x09, // quantity
+      ])
+      const expectedCoils = Buffer.from([0xFF, 0x55, 0x55])
 
+      socket.write = (response) => {
+        assert.deepEqual(expectedCoils, server.coils)
+        done()
+      }
       socket.emit('connection', socket)
       socket.emit('data', request)
-
-      assert.deepEqual(Buffer.from([0xFF, 0x55, 0x55]), server.coils)
-      done()
     })
-    it('should correctly write <0F> in the server buffer coils at address 6', function (done) {
-      let request = Buffer.from([
+    it('should write <0F> in the server buffer coils at address 6', function (done) {
+      const request = Buffer.from([
         0x00, 0x01, // transaction id
         0x00, 0x00, // protocol
         0x00, 0x08, // byte count
-        0x02,       // unit id
-        0x0F,       // function code
+        0x02, // unit id
+        0x0F, // function code
         0x00, 0x06, // address
         0x00, 0x04, // quantity
-        0x01,       // byte count
-        0x0F        // values
+        0x01, // byte count
+        0x0F // values
       ])
+      const expectedResponse = Buffer.from([
+        0x00, 0x01, // transaction id
+        0x00, 0x00, // protocol
+        0x00, 0x06, // byte count
+        0x02, // unit id
+        0x0F, // function code
+        0x00, 0x06, // address
+        0x00, 0x04, // quantity
+      ])
+      const expectedCoils = Buffer.from([0xD5, 0x57, 0x55])
+
+      socket.write = (response) => {
+        assert.deepEqual(expectedResponse, response)
+        assert.deepEqual(expectedCoils, server.coils)
+        done()
+      }
 
       socket.emit('connection', socket)
       socket.emit('data', request)
-
-      assert.deepEqual(Buffer.from([0xD5, 0x57, 0x55]), server.coils)
-      done()
     })
-    it('should correctly write <0F> in the server buffer coils at address 8', function (done) {
-      let request = Buffer.from([
+    it('should write <0F> in the server buffer coils at address 8', function (done) {
+      const request = Buffer.from([
         0x00, 0x01, // transaction id
         0x00, 0x00, // protocol
         0x00, 0x08, // byte count
-        0x02,       // unit id
-        0x0F,       // function code
+        0x02, // unit id
+        0x0F, // function code
         0x00, 0x08, // address
         0x00, 0x04, // quantity
-        0x01,       // byte count
-        0x0F        // values
+        0x01, // byte count
+        0x0F // values
       ])
+      const expectedResponse = Buffer.from([
+        0x00, 0x01, // transaction id
+        0x00, 0x00, // protocol
+        0x00, 0x06, // byte count
+        0x02, // unit id
+        0x0F, // function code
+        0x00, 0x08, // address
+        0x00, 0x04, // quantity
+      ])
+      const expectedCoils = Buffer.from([0x55, 0x5F, 0x55])
+
+      socket.write = (response) => {
+        assert.deepEqual(expectedResponse, response)
+        assert.deepEqual(expectedCoils, server.coils)
+        done()
+      }
 
       socket.emit('connection', socket)
       socket.emit('data', request)
-
-      assert.deepEqual(Buffer.from([0x55, 0x5F, 0x55]), server.coils)
-      done()
     })
   })
 
