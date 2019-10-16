dc2b19b4ff3d7ae10eaa04a9934ad8ae0ac573b4
Alexander Buczynsky <alexander.buczynsky@siemens.com>
2019-10-16 11:07:58 -0500

    consolidated error checkers

--- src/errors/index.ts
+++ src/errors/index.ts
@@ -1,2 +1,4 @@
 export * from './isInternalException'
 export * from './isUserRequestError'
+export { isExceptionResponseBody } from '../response/exception'
+export { isExceptionRequestBody } from '../request/exception'
